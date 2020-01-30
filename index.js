ac41ba302c8285d779cf8d41ca1bcbb13d0c4634
skrnvn <skrnvn@gmail.com>
2020-01-30 20:04:48 +0200

    Napilila json

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var currencies = {
-    rub: {
-        "buy": 0.28000,
-        "sale": 0.32000
-    },
-    eur: {
-        "buy": 19.20000,
-        "sale": 20.00000
-    },
-    usd: {
-        "buy": 15.50000,
-        "sale": 15.85000
+    // rub: {
+    //     "buy": 0.28000,
+    //     "sale": 0.32000
+    // },
+    // eur: {
+    //     "buy": 19.20000,
+    //     "sale": 20.00000
+    // },
+    // usd: {
+    //     "buy": 15.50000,
+    //     "sale": 15.85000
+    // }
+}
+
+var ajax = new XMLHttpRequest();
+ajax.open("get", "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5", false);
+ajax.onload=loadCurrencies;
+ajax.send();
+
+function loadCurrencies(){
+    var currenciesScope = JSON.parse(this.responseText);
+    console.log(currenciesScope)
+    
+    for (i = 0; i < currenciesScope.length; i++) {
+        currency = currenciesScope[i]
+        currencies[currency.ccy.toLowerCase()] = {buy: currency.buy, sale: currency.sale}
     }
+    console.log(currencies)
+    
 }
 
 var currencyName = document.querySelector('#currencyName').value;
